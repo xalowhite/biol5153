7009b623021248f862ef63a497644b5d6b964fc3
Carter White <cmw070@uark.edu>
2024-04-01 09:20:47 -0500

    Edited multiply.py

--- multiply.py
+++ multiply.py
@@ -1,5 +1,23 @@
-size = int(input("Enter the size of your table: " ))
-for i in range(1,size+1):
-    for j in range(1, size+1):
-        print('{:4d}'.format(i *j), end = "")
-    print()
+#! /usr/bin/env python3 
+
+def get_input():
+    size = int(input("Enter the size of your table: " ))
+    return size
+
+
+def print_table(n):
+
+    cell_width = len(str((n * n))) + 1
+
+    for i in range(1,n+1):
+        for j in range(1, n+1):
+            print("{:>{cell_width}}".format(i * j, cell_width = cell_width), end='')
+        print()
+    
+def main():
+    input_num = get_input()
+    
+    print_table(input_num)
+    
+if __name__ == '__main__':
+    main()
